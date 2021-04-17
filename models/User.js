b3b90db271c62325a5b1c2c53c51fdaaad680449
llennairek <llennairek@gmail.com>
2021-04-17 17:56:40 +0200

    updated model and route signin for user

--- models/User.js
+++ models/User.js
@@ -1,19 +1,23 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  email: {
-    unique: true,
-    type: String,
-  },
-  username: {
-    require: true,
-    type: String,
-  },
-  name: String,
-  description: String,
+  email: { type: String, required: true, unique: true },
   token: String,
   hash: String,
   salt: String,
+  account: {
+    username: { type: String, required: true, unique: true },
+    name: String,
+    description: String,
+    photo: Object,
+  },
+  rooms: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Room",
+    },
+  ],
+  // "rooms" permettra de stocker toutes les références vers les annonces créées par l'utilisateur
 });
 
 const User = mongoose.model("User", userSchema);
--- routes/user.js
+++ routes/user.js
@@ -5,23 +5,38 @@ const sha256 = require("crypto-js/sha256");
 const encBase64 = require("crypto-js/enc-base64");
 
 router.post("/sign_up", async (req, res) => {
-  const { password, email, username, name, description } = req.fields;
   try {
-    if (password && email && username && name && description) {
-      const exist = await User.findOne({ email });
-      if (!exist) {
+    const userEmail = await User.findOne({ email: req.fields.email });
+
+    const userUsername = await User.findOne({
+      "account.username": req.fields.username,
+    });
+
+    // const exist = await User.findOne({ email });
+    if (!userEmail && !userUsername) {
+      const { password, email, username, name, description } = req.fields;
+
+      if (password && email && username && name && description) {
         const token = uid2(64);
         const salt = uid2(64);
         const hash = sha256(salt + password).toString(encBase64);
 
-        const newUser = new User({ email, username, name, description, token, salt, hash });
+        const newUser = new User({
+          email,
+          token,
+          salt,
+          hash,
+          account: { username, name, description },
+        });
         await newUser.save();
         res.status(201).json({ id: newUser.id, token, email, username, description, name });
       } else {
-        res.status(400).json({ error: "This email already has an account." });
+        res.status(400).json({ error: "Missing parameters" });
       }
-    } else {
-      res.status(400).json({ error: "Missing parameters" });
+    } else if (userEmail) {
+      res.status(400).json({ error: "This email already has an account." });
+    } else if (userUsername) {
+      res.status(400).json({ error: "This username already has an account." });
     }
   } catch (error) {
     res.status(500).json({ erorr: error.message });
@@ -40,9 +55,9 @@ router.post("/log_in", async (req, res) => {
             _id: user.id,
             token: user.token,
             email: user.email,
-            username: user.username,
-            description: user.description,
-            name: user.name,
+            username: user.account.username,
+            description: user.account.description,
+            name: user.account.name,
           });
         } else {
           res.status(400).json({ error: "Unauthorized" });
